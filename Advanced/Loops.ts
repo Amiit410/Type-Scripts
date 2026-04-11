c0b86a4660b18a312bb715f9e0b97383cbd812d2
amitkumar linge <amitlinge410@gmail.com>
2026-04-11 17:39:34 +0530

    loops in ts

---
+++ Advanced/Loops.ts
@@ -0,0 +1,29 @@
+//// while loop
+// var start : number = 1;
+// while(start<=5){
+//     console.log(start);
+//     start++;
+// }
+
+// // do-while loop
+// var start: number = 9;
+// do {
+//     console.log(start);
+//     start++;
+// } while (start <= 5)
+
+// //for loop
+// for (var i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// for in loop
+var arr : number[] = [10,20,30,40,50];
+
+for(var i in arr){
+    console.log(i);     //prints index number
+}
+
+for(var j of arr){
+    console.log(j);     //prints value
+}
